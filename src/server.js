eb7b8089fab1fec9cbcecd9649e77f419b7bac0e
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-06-27 22:15:24 +0530

    updates

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import connectDb from './config/db.js'
 import app from './index.js'
 
-
 const port = 8000
+
 app.listen(port , async () => {
     await connectDb();
     console.log("app is running on port ",port)
